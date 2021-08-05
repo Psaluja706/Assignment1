ac0ed2908395150cf98c81a8a05e651ded917d78
Psaluja706 <priyanshu1231.cse19@chitkara.edu.in>
2021-08-05 20:36:48 +0530

    Files Added

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TwoStacksProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDoubleStack<int> s = new DoubleStack<int>(6);
+           
+            s.Push1(8);
+            s.Push2(10);
+            s.Push1(7);
+            s.Push1(5);
+            s.Push2(20);
+            s.Push2(30);
+            Console.WriteLine(s.count1());
+            Console.WriteLine(s.count2());
+            s.print1();
+            s.print2();
+
+
+
+
+        }
+    }
+}
